e58f8f635d003b64e9f4f8dfb7ec75be43041f4c
Adarsha <learnadarsha@gmail.com>
2020-06-26 10:10:53 +0545

    dot and arrow navigate with automate slider

--- carousel.js
+++ carousel.js
@@ -6,10 +6,13 @@ class ImageCarousel {
     this.totalImages = this.imagesContainer.children.length;
     this.totalwidth = this.imagesContainerWidth * this.totalImages;
     this.imagesContainer.style.width = this.totalwidth + 'px';
+    this.leftArrow = document.createElement('div');
+    this.rightArrow = document.createElement('div');
+    this.dotsContainer = document.createElement('div');
     this.dotCollection = [];
     this.transitionTime = 1;
     this.holdTime = 3000;
-    this.speed = 5;
+    this.speed = 10;
     this.left = 0;
     this.index = 0;
     this.newIndex = 0;
@@ -18,62 +21,113 @@ class ImageCarousel {
   init() {
     this.createDots();
     this.createArrows();
-    this.initializeSlider();
+    this.indicateDot();
+    this.automateSlider();
+    this.dotNavigate();
+    this.arrowNavigate();
   }
 
   createDots() {
-    const dotsContainer = document.createElement('div');
-    dotsContainer.setAttribute('style', 'transform:translateX(-50%); position:absolute; z-index:1; bottom:15px; left:50%;');
-    this.carouselframe.appendChild(dotsContainer);
+    this.dotsContainer.setAttribute('style', 'transform:translateX(-50%); position:absolute; z-index:1; bottom:15px; left:50%;');
+    this.carouselframe.appendChild(this.dotsContainer);
 
-    for(let i=0; i<this.totalImages; i++){
-      const dot = document.createElement('div');
+    for (let i = 0; i < this.totalImages; i++) {
+      const dot = document.createElement('span');
       dot.setAttribute('style', 'height:10px; width:10px; border:2px solid black; border-radius:50%; background:white; display:inline-block; margin:5px; cursor:pointer;');
-      dotsContainer.appendChild(dot);
+      this.dotsContainer.appendChild(dot);
       this.dotCollection.push(dot);
     }
   }
 
   createArrows() {
-    const leftArrow = document.createElement('div');
-    const rightArrow = document.createElement('div');
-    leftArrow.setAttribute('style', 'position:absolute; top:50%; transform:translateY(-50%) rotate(-90deg); left:15px; z-index:1; font-weight:bolder; font-size: 3rem; color:white; opacity:85%; cursor:pointer;')
-    rightArrow.setAttribute('style', 'position:absolute; top:50%; transform:translateY(-50%) rotate(90deg); right:15px; z-index:1; font-weight:bolder; font-size: 3rem; color:white; opacity:85%; cursor:pointer;')
-    leftArrow.textContent = '^';
-    rightArrow.textContent = '^';
-    this.carouselframe.appendChild(leftArrow);
-    this.carouselframe.appendChild(rightArrow);
+    this.leftArrow.setAttribute('style', 'position:absolute; top:50%; transform:translateY(-50%) rotate(-90deg); left:15px; z-index:1; font-weight:bolder; font-size: 3rem; color:white; opacity:85%; cursor:pointer;')
+    this.rightArrow.setAttribute('style', 'position:absolute; top:50%; transform:translateY(-50%) rotate(90deg); right:15px; z-index:1; font-weight:bolder; font-size: 3rem; color:white; opacity:85%; cursor:pointer;')
+    this.leftArrow.textContent = '^';
+    this.rightArrow.textContent = '^';
+    this.carouselframe.appendChild(this.leftArrow);
+    this.carouselframe.appendChild(this.rightArrow);
   }
 
-  slideImage(index, newIndex){
-    const moveImage = setInterval(() => {
-      this.left = this.left + this.speed * (newIndex-index);
-      
-      if (this.left === this.imagesContainerWidth * newIndex){
-        clearInterval(moveImage);
-        this.initializeSlider();
-      }
-      this.imagesContainer.style.left = -this.left + 'px'; 
-      
-      
-    },this.transitionTime);
-
-  }
-
-  initializeSlider(){
-    setTimeout(() => {
-      this.slideImage(this.index, this.newIndex);
-    }, 5000);
-    
+  indicateDot(){
     this.dotCollection[this.index].style.background = 'white';
     this.dotCollection[this.newIndex].style.background = 'black';
-    this.index = this.newIndex;
-    this.newIndex ++;
-    
-    if(this.newIndex === this.totalImages){
-      this.newIndex = 0;
-    }
-    
+  }
+
+  slideImage(index, newIndex) {
+    this.moveImage = setInterval(() => {
+      if (this.left < 0){
+        this.left = 0;
+      }
+      if (this.left > this.totalwidth - this.imagesContainerWidth){
+        this.left = this.totalwidth - this.imagesContainerWidth
+      }
+
+      this.left = this.left + this.speed * (newIndex - index);
+      console.log(this.left);
+      if (this.left === this.imagesContainerWidth * newIndex) {
+        clearInterval(this.moveImage);
+      }
+      
+      this.imagesContainer.style.left = -this.left + 'px';
+      
+    }, this.transitionTime);
+  }
+
+
+  dotNavigate() {
+    this.dotsContainer.addEventListener('click', e => {
+      clearInterval(this.moveImage);
+      clearInterval(this.holdImage);
+      const targetDot = e.target.closest('span');
+      if (!targetDot) return;
+
+      const targetIndex = this.dotCollection.findIndex(dot => dot === targetDot);
+      this.index = this.newIndex;
+      this.newIndex = targetIndex;
+      this.slideImage(this.index, this.newIndex);
+      this.automateSlider();
+      this.indicateDot();
+    });
+  }
+
+  arrowNavigate(){
+    this.leftArrow.addEventListener('click', () => {
+      clearInterval(this.moveImage);
+      clearInterval(this.holdImage);
+      this.index = this.newIndex;
+      this.newIndex--
+      if(this.newIndex < 0){
+        this.newIndex = this.totalImages - 1;
+      }
+      this.slideImage(this.index, this.newIndex);
+      this.automateSlider();
+      this.indicateDot();
+    });
+    this.rightArrow.addEventListener('click', () => {
+      clearInterval(this.moveImage);
+      clearInterval(this.holdImage);
+      this.index = this.newIndex;
+      this.newIndex++;
+      if(this.newIndex === this.totalImages){
+        this.newIndex = 0;
+      }
+      this.slideImage(this.index, this.newIndex);
+      this.automateSlider();
+      this.indicateDot();
+    });
+  }
+
+  automateSlider() {
+    this.holdImage = setInterval(() => {
+
+      this.index = this.newIndex;
+      this.newIndex++;
+      if (this.newIndex === this.totalImages){
+        this.newIndex = 0;
+      }
+      this.slideImage(this.index, this.newIndex);
+      this.indicateDot();
+    }, this.holdTime);
   }
 
 }
